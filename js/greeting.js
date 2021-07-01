32c40f1ea37d306299ef6a0ba1755a82fa6069ee
Jinho Jeong <jhjeong710@gmail.com>
2021-07-01 23:28:17 +0900

    refactoring

--- js/greeting.js
+++ js/greeting.js
@@ -1,30 +1,74 @@
 const loginInput=document.querySelector("#login-form input");
 const loginForm=document.querySelector("#login-form");
 const h1Title=document.querySelector("#greeting");
+const modiName=document.querySelector("#greetingDiv button");
 
 const HIDDEN_CLASSNAME="hidden";
 const USERNAME_KEY="username";
 
+//로그인 되어 있는지 체크
+//local Storage도 같이 체크한다.
+const username=(function username(name){
+    let username=name?name:"";
+    const init=(function(){
+        const lsName=localStorage.getItem(USERNAME_KEY);
+        if(lsName!=null){
+            username=lsName;
+        }
+    })();
+    return {
+        setName:function(name) {
+            username=name;
+        },
+        getName:function(){
+            return username;
+        },
+        isLogin:function(){
+            if(username=="") {
+                return false;
+            }
+            else{
+                return true;
+            }
+        }
+    }
+})();
+
+//새로고침 이후 페이지 init
+const initialLoaded=(function pageInit(){
+    username();
+    if(!username.isLogin){
+        paintLogin();
+    }
+    else{
+        paintLogin(username.getName());
+        
+    }
+})();
+
+
 function onLoginSubmit(events){
     events.preventDefault();
     const username=loginInput.value;
-    loginForm.classList.add("hidden");
+    loginForm.classList.add(HIDDEN_CLASSNAME);
     localStorage.setItem(USERNAME_KEY,username);
     paintGreeting(username);
 }
 
 function paintGreeting(username){
-    h1Title.innerText=`Hello ${username}`;
+    const date=new Date();
+    const hour=date.getTime();
+    let greetingMsg="";
+    if(hour<11) {greetingMsg="Good morning";}
+    else if(hour<18) {greetingMsg="Good afternoon";}
+    else greetingMsg="Good evening";
+
+    h1Title.innerText=`${greetingMsg} ${username}`;
     h1Title.classList.remove(HIDDEN_CLASSNAME);
 }
 
-
-const savedUsername=localStorage.getItem(USERNAME_KEY);
-
-if(savedUsername===null){
+function paintLogin(){
     loginForm.classList.remove("hidden");
     loginForm.addEventListener("submit",onLoginSubmit); 
 }
-else{
-    paintGreeting(savedUsername);
-}
+
--- js/todo.js
+++ js/todo.js
@@ -2,11 +2,45 @@ const todoForm=document.getElementById("todo-form");
 const todoList=document.getElementById("todo-list");
 const todoInput=document.querySelector("#todo-form input");
 
+const TODOS_KEY="todos";
+
+const todoArray=function todoArray(){
+    let todos=[];
+    return {
+        setArray:function(array){
+            todos=array;
+        },
+        addElement:function(element){
+            todos.push(element);
+        },
+        initArray:function(){
+            const get=localStorage.getItem(TODOS_KEY);
+            if(get!==null){
+                const parseArray=JSON.parse(get);
+                console.log(parseArray);
+                todos=parseArray;
+            }
+        },
+        syncArray:function(){
+            localStorage.setItem(TODOS_KEY,JSON.stringify(todos));
+        },
+        getArray:function(){
+            return todos;
+        },
+        //수정 필요
+        deleteArray:function(){
+            todos.delete
+        }
+    }
+}();
+
 function handleToDoSubmit(event){
     event.preventDefault();
     const newTodo=todoInput.value;
     todoInput.value="";
     paintToDo(newTodo);
+    todoArray.addElement(newTodo);
+    todoArray.syncArray();
 }
 
 function paintToDo(newTodo){
@@ -22,8 +56,17 @@ function paintToDo(newTodo){
 }
 
 function deleteToDo(event){
-    console.log(event.target.parentNode.innerText);
+    const deleteObject=event.target.parentNode;
+    console.log(event.target.parentNode);
+    deleteObject.remove();
+
 }
+
+const todoInit=(function(){
+    const todos=todoArray.getArray();
+    console.log(todos);
+    todos.forEach(paintToDo);
+})();
 
 todoForm.addEventListener("submit",handleToDoSubmit);
 
